ddc4c62a1effc10e5ff26f8cc6894c60adb3f2ec
高橋礼 <graphicocolo@gmail.com>
2023-01-27 15:32:52 +0900

    ライフサイクルと副作用を理解しよう

--- react-basic/src/components/Counter.jsx
+++ react-basic/src/components/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0)
@@ -8,6 +8,27 @@ const Counter = () => {
   const countDown = () => {
     setCount(prevState => prevState - 1)
   }
+
+  // レンダリングのたび毎回実行される
+  useEffect(() => {
+    console.log('Current count is...', count)
+  })
+  
+  // 初回レンダリング後のみ実行される
+  // 再実行されることはないが初回のみ必ず実行される
+  // useEffect(() => {
+  //   console.log('Current count is...', count)
+  // }, [])
+  
+  // trigger が変更されるたび実行される
+  // useEffect(() => {
+  //   console.log('Current count is...', count)
+  // }, [trigger])
+  
+  // triggaer1 か triggaer2 どちらかが変更されるたび実行される
+  // useEffect(() => {
+  //   console.log('Current count is...', count)
+  // }, [trigger1, trigger2])
 
   return (
     <div>
--- react-basic/src/components/ToggleButton.jsx
+++ react-basic/src/components/ToggleButton.jsx
@@ -1,10 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const ToggleButton = () => {
   const [open, setOpen] = useState(false)
   const toggle = () => {
     setOpen(prevState => !prevState)
   }
+
+  useEffect(() => {
+    console.log('Current state is', open)
+    if (open) {
+      console.log('Subscribe detabase...')
+    }
+    // クリーンアップ関数
+    // この中でデータベース購読解除を行ったりする
+    // 再レンダリングが実行される直前に必ず実行される仕組み
+    return () => {
+      console.log('Unsubscribe database!')
+    }
+  })
 
   return (
     <div>
