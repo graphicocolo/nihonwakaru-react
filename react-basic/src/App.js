01fae5dd1da9a616cdb8d167799bbf1083914752
高橋礼 <graphicocolo@gmail.com>
2023-01-28 15:20:46 +0900

    useEffect内でAPIを呼び出そう

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,10 +1,33 @@
 // import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 import {Article, TextInput, Counter, BadCounter, ToggleButton} from './components';
 
 function App() {
+  const [name, setName] = useState('')
+  const [id, setId] = useState('deatiger')
+  const ids = ['deatiger', 'gaearon', 'aws', 'google', 'facebook']
+  const getRandomId = () => {
+    const _id = ids[Math.floor(Math.random() * ids.length)]
+    setId(_id)
+  }
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${id}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      setName(data.name)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }, [id])
+
   return (
     <div>
+      <p>{id}の、GitHub 上の名前は{name}です。</p>
+      <button onClick={getRandomId}>IDを変更</button>
       <Article 
         title={'import と export'}
         content={'今日のトピックスです。名前つきimport・export、default import・exportの実装ができたでしょうか？'}
